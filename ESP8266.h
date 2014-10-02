1c3825dc2d2b4e40969b0806e07f08eb2b9e40e4
henols <henols@gmail.com>
2014-10-02 11:46:38 +0200

    d

--- ESP8266.h
+++ ESP8266.h
@@ -2,18 +2,18 @@
 /*! 
   @file     ESP8266.h
   @author   KTOWN (Kevin Townsend for Adafruit Industries)
-  @license  BSD (see license.txt) 
+  @license  BSD (see license.txt)
 
   This is a library for the Adafruit CC3000 WiFi breakout board
   This library works with the Adafruit CC3000 breakout
   ----> https://www.adafruit.com/products/1469
-  
-  Check out the links above for our tutorials and wiring diagrams 
+
+  Check out the links above for our tutorials and wiring diagrams
   These chips use SPI to communicate.
-  
-  Adafruit invests time and resources providing this open source code, 
-  please support Adafruit and open-source hardware by purchasing 
-  products from Adafruit! 
+
+  Adafruit invests time and resources providing this open source code,
+  please support Adafruit and open-source hardware by purchasing
+  products from Adafruit!
 */
 /**************************************************************************/
 
@@ -36,9 +36,14 @@
 #define WIFI_DISABLE 0
 #define WIFI_STATUS_CONNECTED 1
 
+#define      WLAN_SEC_UNSEC (0)
+#define      WLAN_SEC_WEP	(1)
+#define      WLAN_SEC_WPA	(2)
+#define      WLAN_SEC_WPA2	(3)
+
 
 /* Enum for wlan_ioctl_statusget results */
-typedef enum 
+typedef enum
 {
   STATUS_DISCONNECTED = 0,
   STATUS_SCANNING     = 1,
@@ -53,7 +58,7 @@ class ESP8266_Client : public Client {
 	ESP8266_Client(ESP8266* esp8266, int16_t id);
 	ESP8266_Client(void);
   void operator=(const ESP8266_Client& other);
-  
+
   int connect(IPAddress ip, uint16_t port);
   int connect(const char *host, uint16_t port);
 
@@ -98,7 +103,7 @@ class ESP8266 {
 
     bool     connectToAP(const char *ssid, const char *key, uint8_t secmode, uint8_t attempts = 0);
     bool     connectSecure(const char *ssid, const char *key, int32_t secMode);
-    bool     connectOpen(const char *ssid); 
+    bool     connectOpen(const char *ssid);
     bool     checkConnected(void);
 
     bool     getIPAddress(uint32_t *retip, uint32_t *netmask, uint32_t *gateway, uint32_t *dhcpserv, uint32_t *dnsserv);
@@ -106,7 +111,7 @@ class ESP8266 {
 
     ESP8266_Client connectTCP(uint32_t destIP, uint16_t destPort);
     ESP8266_Client connectUDP(uint32_t destIP, uint16_t destPort);
-     
+
     bool     getFirmwareVersion(uint8_t *major, uint8_t *minor);
     status_t getStatus(void);
 
